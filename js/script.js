830feba351e677b0e1011487921e269a3a3604f4
Николай Васильев <60035927+Kolyanbodymass@users.noreply.github.com>
2020-05-08 21:42:35 +0500

    add travel cost calculator

--- js/script.js
+++ js/script.js
@@ -229,4 +229,44 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // Calc
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+        totalValue.innerHTML = 0;
+
+        persons.addEventListener('change', function() {
+            personsSum = +this.value;
+            if (restDays.value !== '' && persons.value !== '') {
+                total = (daysSum + personsSum) * 4000;
+                totalValue.innerHTML = total;
+            } else {
+                totalValue.innerHTML = 0;
+            }           
+        });
+
+        restDays.addEventListener('change', function() {
+            daysSum = +this.value;
+            if (restDays.value !== '' && persons.value !== '') {
+                total = (daysSum + personsSum) * 4000;
+                totalValue.innerHTML = total;
+            } else {
+                totalValue.innerHTML = 0;
+            }
+        });
+
+        place.addEventListener('change', function() {
+            if (restDays.value == '' || persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                let a = total;
+                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+            }
+        });
 });
